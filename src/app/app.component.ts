aec24f66371b3a0f4e47835ef94996a53165a07c
gargigajjar <ggajjar@mdixinc.com>
2025-01-24 15:58:00 -0500

    Added navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CreateServiceComponent } from './create-service/create-service.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CreateServiceComponent],
+  imports: [RouterOutlet, NavbarComponent],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
